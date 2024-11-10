c720d508f080c235234a464a1c5ca71c3a7643a4
Neethamadhu Madurasinghe <neethamadhu.ma@gmail.com>
2024-11-10 21:43:10 +0530

    Initial_experiment_setup

--- hand_detector.py
+++ hand_detector.py
@@ -2,80 +2,93 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import os
+import asyncio
 
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
-mp_holistic = mp.solutions.holistic
 
+video_path = './inputs/'
+output_dir = './all_outputs/outputs'
+video_files = []
 
-# Initialize Mediapipe holistic and drawing utilities
-mp_holistic = mp.solutions.holistic
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
+def read_all_files(path):
+    # Read all the file names in video_path directory and add them to video_files array with video_path prepend to file name eg: inputs/video.mp4
+    for filename in os.listdir(path):
+        video_files.append(os.path.join(path, filename))
 
-# Specify the path to your video file
-video_path = './inputs/w00720221216120558.mp4'
+async def detect(video_path):
+    # Extract the file name from the video path and create the output file path
+    video_filename = os.path.basename(video_path)
+    output_path = os.path.join(output_dir, video_filename)
 
-# Create the outputs directory if it doesn't exist
-output_dir = 'outputs'
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+    # Capture the video
+    cap = cv2.VideoCapture(video_path)
 
-# Extract the file name from the video path and create the output file path
-video_filename = os.path.basename(video_path)
-output_path = os.path.join(output_dir, video_filename)
+    # Get the width, height, and frames per second (fps) of the input video
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
 
-# Capture the video
-cap = cv2.VideoCapture(video_path)
+    # Define the codec and create a VideoWriter object to save the output video
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Codec for mp4
+    out = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
 
-# Get the width, height, and frames per second (fps) of the input video
-frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fps = int(cap.get(cv2.CAP_PROP_FPS))
+    with mp_hands.Hands(
+        model_complexity=0,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as hands:
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                print("Ignoring empty frame or end of video.")
+                break
 
-# Define the codec and create a VideoWriter object to save the output video
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Codec for mp4
-out = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
+            # To improve performance, optionally mark the image as not writeable to pass by reference.
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = hands.process(image)
 
-with mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty frame or end of video.")
-      # If loading a video, use 'break' instead of 'continue'.
-      break
+            # Draw the hand annotations on the image.
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(
+                        image,
+                        hand_landmarks,
+                        mp_hands.HAND_CONNECTIONS,
+                        mp_drawing_styles.get_default_hand_landmarks_style(),
+                        mp_drawing_styles.get_default_hand_connections_style())
+                
+            out.write(image)
+            
+            # Optionally, show the video (for debugging purposes)
+            # Flip the image horizontally for a selfie-view display.
+            cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+            
+        cap.release()
+        out.release()
+        cv2.destroyAllWindows()
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
+    print(f"Processed video saved as: {output_path}")
 
-    # Draw the hand annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
+async def main():
+    read_all_files(video_path)
+
+    print("Found " + str(len(video_files)) + " videos")
+
+    # Create the outputs directory if it doesn't exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
         
-    out.write(image)    
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
+    # Process each video one by one
+    for video_file in video_files:
+        print("Converting video: " + video_file)
+        await detect(video_file)
+        
+    print("Completed all " + str(len(video_files)) + " videos")
 
-
-cap.release()
-out.release()
-cv2.destroyAllWindows()
-
-print(f"Processed video saved as: {output_path}")
+asyncio.run(main())
---
+++ holistic_and_hand.py
@@ -0,0 +1,195 @@
+import cv2
+import mediapipe as mp
+import os
+import asyncio
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_holistic = mp.solutions.holistic
+mp_hands = mp.solutions.hands
+
+
+video_path = './inputs/'
+output_dir = './all_outputs/output_combined'
+video_files = []
+
+def read_all_files(path):
+    # Read all the file names in video_path directory and add them to video_files array with video_path prepend to file name eg: inputs/video.mp4
+    for filename in os.listdir(path):
+        video_files.append(os.path.join(path, filename))
+
+async def detect(video_path):
+    # Extract the file name from the video path and create the output file path
+    video_filename = os.path.basename(video_path)
+    output_path = os.path.join(output_dir, video_filename)
+
+    # Capture the video
+    cap = cv2.VideoCapture(video_path)
+
+    # Get the width, height, and frames per second (fps) of the input video
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+    # Define the codec and create a VideoWriter object to save the output video
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Codec for mp4
+    out = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
+    
+    
+    ###########
+    
+    with mp_holistic.Holistic(
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as holistic:
+        
+        with mp_hands.Hands(
+            model_complexity=0,
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5) as hands:
+            
+            while cap.isOpened():
+                success, image = cap.read()
+                if not success:
+                    print("Ignoring empty frame or end of video.")
+                    break
+                
+                image.flags.writeable = False
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            
+                results_holistic = holistic.process(image)
+                results_hands = hands.process(image)
+                
+                print(results_holistic)
+    
+                
+                if results_holistic.face_landmarks and results_holistic.pose_landmarks:
+                    mp_drawing.draw_landmarks(
+                        image,
+                        results_holistic.face_landmarks,
+                        mp_holistic.FACEMESH_CONTOURS,
+                        landmark_drawing_spec=None,
+                        connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_contours_style()
+                    )
+                    
+                    mp_drawing.draw_landmarks(
+                        image,
+                        results_holistic.pose_landmarks,
+                        mp_holistic.POSE_CONNECTIONS,
+                        landmark_drawing_spec=mp_drawing_styles
+                        .get_default_pose_landmarks_style()
+                    )
+                else:
+                    print("Error: Could not extract landmarks using holistic model")
+                    
+    
+
+                # Draw the hand annotations on the image.
+                if results_hands.multi_hand_landmarks:
+                    for hand_landmarks in results_hands.multi_hand_landmarks:
+                        mp_drawing.draw_landmarks(
+                            image,
+                            hand_landmarks,
+                            mp_hands.HAND_CONNECTIONS,
+                            mp_drawing_styles.get_default_hand_landmarks_style(),
+                            mp_drawing_styles.get_default_hand_connections_style())
+                else:
+                    print("Error: Could not extract hand landmarks")               
+                    
+                    
+                out.write(image)
+                cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+                if cv2.waitKey(5) & 0xFF == 27:
+                    break    
+            
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()        
+    
+    ###########
+
+    # with mp_holistic.Holistic(
+    #     min_detection_confidence=0.5,
+    #     min_tracking_confidence=0.5) as holistic:
+    #     while cap.isOpened():
+    #         success, image = cap.read()
+    #         if not success:
+    #             print("Ignoring empty frame or end of video.")
+    #             break
+
+    #         # To improve performance, optionally mark the image as not writeable to pass by reference.
+    #         image.flags.writeable = False
+    #         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    #         results = holistic.process(image)
+            
+            
+    #          # Draw landmark annotation on the image.
+    #         image.flags.writeable = True
+    #         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    #         mp_drawing.draw_landmarks(
+    #             image,
+    #             results.face_landmarks,
+    #             mp_holistic.FACEMESH_CONTOURS,
+    #             landmark_drawing_spec=None,
+    #             connection_drawing_spec=mp_drawing_styles
+    #             .get_default_face_mesh_contours_style())
+    #         mp_drawing.draw_landmarks(
+    #             image,
+    #             results.pose_landmarks,
+    #             mp_holistic.POSE_CONNECTIONS,
+    #             landmark_drawing_spec=mp_drawing_styles
+    #             .get_default_pose_landmarks_style())
+            
+            
+    #         # Extract hand landmarks
+            
+    #         with mp_hands.Hands(
+    #         model_complexity=0,
+    #         min_detection_confidence=0.5,
+    #         min_tracking_confidence=0.5) as hands:
+    #             results_hands = hands.process(image)
+    #             print(results_hands)
+
+    #             # Draw the hand annotations on the image.
+    #             if results_hands.multi_hand_landmarks:
+    #                 for hand_landmarks in results_hands.multi_hand_landmarks:
+    #                     mp_drawing.draw_landmarks(
+    #                         image,
+    #                         hand_landmarks,
+    #                         mp_hands.HAND_CONNECTIONS,
+    #                         mp_drawing_styles.get_default_hand_landmarks_style(),
+    #                         mp_drawing_styles.get_default_hand_connections_style())
+    #             else:
+    #                 print("Error: Could not extract hand landmarks")       
+                        
+                        
+    #             out.write(image)
+                            
+    #             # Optionally, show the video (for debugging purposes)
+    #             # Flip the image horizontally for a selfie-view display.
+    #             cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+    #             if cv2.waitKey(5) & 0xFF == 27:
+    #                 break
+            
+    #     cap.release()
+    #     out.release()
+    #     cv2.destroyAllWindows()
+
+    print(f"Processed video saved as: {output_path}")
+
+async def main():
+    read_all_files(video_path)
+
+    print("Found " + str(len(video_files)) + " videos")
+
+    # Create the outputs directory if it doesn't exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+        
+    # Process each video one by one
+    for video_file in video_files:
+        print("Converting video: " + video_file)
+        await detect(video_file)
+        
+    print("Completed all " + str(len(video_files)) + " videos")
+
+asyncio.run(main())
